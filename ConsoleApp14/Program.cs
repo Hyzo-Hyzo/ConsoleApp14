38a27a63a01e223174919eb3cedd842d40bf32c5
Ira <mazuririnochkacat@gmail.com>
2023-05-30 20:00:57 +0300

    all

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace ConsoleApp14
+﻿using System.Linq;
+
+namespace ConsoleApp14
 {
-    class Program
+
+    class Car
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+
+        public Car(string name, string manufacturer, int year)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Year = year;
+        }
+    }
+
+    class CarComparer : IEqualityComparer<Car>
+    {
+        public bool Equals(Car x, Car y)
+        {
+            return x.Manufacturer == y.Manufacturer;
+        }
+
+        public int GetHashCode(Car obj)
+        {
+            return obj.Manufacturer.GetHashCode();
+        }
+    }
+        class Program
     {
         static void Main()
         {
+            //3 частина (1-3)
             int[] array = { 2, 4, 6, 8, 10, 9, 14, 18, 21, 945, 1000 };
 
             CalculateProduct(array);
@@ -20,6 +50,59 @@
             DisplayNumberStats(array);
             DisplayEvenNumberStats(array);
             DisplayEvenOddNumberStats(array);
+            //2 частина номер 1
+            string[] array2 = { "apple", "banana", "cherry",  "elderberry" };
+
+            Console.WriteLine("Сортування за спаданням довжини рядків:");
+            SortByStringLengthDescending(array2);
+
+            Console.WriteLine("Сортування за зростанням довжини рядків:");
+            SortByStringLengthAscending(array2);
+
+            //2 частина номер 2
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+            int[] arr2 = { 4, 5, 6, 7, 8 };
+
+            Console.WriteLine("Різниця двох масивів:");
+            var difference = arr1.Except(arr2);
+            PrintArray(difference);
+
+            Console.WriteLine("Перетин двох масивів:");
+            var intersection = arr1.Intersect(arr2);
+            PrintArray(intersection);
+
+            Console.WriteLine("Об'єднання двох масивів без дублікатів:");
+            var union = arr1.Union(arr2);
+            PrintArray(union);
+
+            Console.WriteLine("Вміст першого масиву без повторень:");
+            var distinct = arr1.Distinct();
+            PrintArray(distinct);
+
+            //2 частина номер 3
+            Car[] cars1 = {
+            new Car("BMW", "BMW Group", 2018),
+            new Car("Toyota Camry", "Toyota", 2019),
+            new Car("Honda Accord", "Honda", 2020)
+                };
+
+            Car[] cars2 = {
+            new Car("BMW", "BMW Group", 2018),
+            new Car("Toyota Corolla", "Toyota", 2021),
+            new Car("Ford Mustang", "Ford", 2017)
+                };
+
+            Console.WriteLine("Різниця двох масивів:");
+            var difference3 = cars1.Except(cars2, new CarComparer());
+            PrintCars(difference3);
+
+            Console.WriteLine("Перетин двох масивів:");
+            var intersection3 = cars1.Intersect(cars2, new CarComparer());
+            PrintCars(intersection3);
+
+            Console.WriteLine("Об'єднання двох масивів:");
+            var union3 = cars1.Union(cars2, new CarComparer());
+            PrintCars(union3);
         }
 
         static void CalculateProduct(int[] array)
@@ -126,5 +209,35 @@
                 Console.WriteLine($"{stat.Type} - {stat.Count} рази");
             }
         }
+
+        static void SortByStringLengthDescending(string[] array2)
+        {
+            var sortedArray = array2.OrderByDescending(str => str.Length);
+            Console.WriteLine(string.Join(", ", sortedArray));
+        }
+
+        static void SortByStringLengthAscending(string[] array2)
+        {
+            var sortedArray = array2.OrderBy(str => str.Length);
+
+            Console.WriteLine(string.Join(", ", sortedArray));
+           
+        }
+
+        static void PrintArray(IEnumerable<int> array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+
+        }
+
+        static void PrintCars(IEnumerable<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"Назва: {car.Name}, Виробник: {car.Manufacturer}, Рік випуску: {car.Year}");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
